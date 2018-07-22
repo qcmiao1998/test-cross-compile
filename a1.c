5051c96ec4c819093b28e34bbce22995c0fbbc23
qcmiao1998 <qcmiao@hotmail.com>
2018-07-22 23:24:22 +0800

    Modify Settings and add lost files.

---
+++ a1.c
@@ -0,0 +1,11 @@
+//
+// Created by qcmiao on 18-7-22.
+//
+
+#include "a1.h"
+#include "stdio.h"
+
+int main(){
+    printf("Test APP!");
+    return 0;
+}
---
+++ a1.h
@@ -0,0 +1,8 @@
+//
+// Created by qcmiao on 18-7-22.
+//
+
+#ifndef PROJECT_A1_H
+#define PROJECT_A1_H
+int main();
+#endif //PROJECT_A1_H
